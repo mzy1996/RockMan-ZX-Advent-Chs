aeda8cdc61a2d579535e64a9c029422eaa5fe6f2
GitHub Wokann <Kenny_chen1998@outlook.com>
2024-02-01 10:17:57 +0800

    文本位置调整

--- strings/strings.s
+++ strings/strings.s
@@ -1,436 +1,438 @@
 .create ".\.temp\root\data\m_back_jp.bin", 0
-    .incbin ".\strings\m_back_jp.bin"
+    .incbin ".\strings\font_jp\m_back_jp.bin"
 .close
 
 .create ".\.temp\root\data\m_mg_jp.bin", 0
-    .incbin ".\strings\m_mg_jp.bin"
+    .incbin ".\strings\font_jp\m_mg_jp.bin"
 .close
 
 .create ".\.temp\root\data\m_over_jp.bin", 0
-    .incbin ".\strings\m_over_jp.bin"
+    .incbin ".\strings\font_jp\m_over_jp.bin"
 .close
 
 .create ".\.temp\root\data\m_sdisk_jp.bin", 0
-    .incbin ".\strings\m_sdisk_jp.bin"
+    .incbin ".\strings\font_jp\m_sdisk_jp.bin"
 .close
 
 .create ".\.temp\root\data\m_sub_jp.bin", 0
-    .incbin ".\strings\m_sub_jp.bin"
+    .incbin ".\strings\font_jp\m_sub_jp.bin"
 .close
 
 .create ".\.temp\root\data\m_sys_jp.bin", 0
-    .incbin ".\strings\m_sys_jp.bin"
+    .incbin ".\strings\font_jp\m_sys_jp.bin"
 .close
 
 .create ".\.temp\root\data\talk_ex0_jp1.bin", 0
-    .incbin ".\strings\talk_ex0_jp1.bin"
+    .incbin ".\strings\font_jp\talk_ex0_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_ex0_jp2.bin", 0
-    .incbin ".\strings\talk_ex0_jp2.bin"
+    .incbin ".\strings\font_jp\talk_ex0_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_hn0_jp1.bin", 0
-    .incbin ".\strings\talk_hn0_jp1.bin"
+    .incbin ".\strings\font_jp\talk_hn0_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_hn0_jp2.bin", 0
-    .incbin ".\strings\talk_hn0_jp2.bin"
+    .incbin ".\strings\font_jp\talk_hn0_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_hn1_jp1.bin", 0
-    .incbin ".\strings\talk_hn1_jp1.bin"
+    .incbin ".\strings\font_jp\talk_hn1_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_hn1_jp2.bin", 0
-    .incbin ".\strings\talk_hn1_jp2.bin"
+    .incbin ".\strings\font_jp\talk_hn1_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_hn2_jp1.bin", 0
-    .incbin ".\strings\talk_hn2_jp1.bin"
+    .incbin ".\strings\font_jp\talk_hn2_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_hn2_jp2.bin", 0
-    .incbin ".\strings\talk_hn2_jp2.bin"
+    .incbin ".\strings\font_jp\talk_hn2_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_hn3_jp1.bin", 0
-    .incbin ".\strings\talk_hn3_jp1.bin"
+    .incbin ".\strings\font_jp\talk_hn3_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_hn3_jp2.bin", 0
-    .incbin ".\strings\talk_hn3_jp2.bin"
+    .incbin ".\strings\font_jp\talk_hn3_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_m01_jp1.bin", 0
-    .incbin ".\strings\talk_m01_jp1.bin"
+    .incbin ".\strings\font_jp\talk_m01_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_m01_jp2.bin", 0
-    .incbin ".\strings\talk_m01_jp2.bin"
+    .incbin ".\strings\font_jp\talk_m01_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_m02_jp2.bin", 0
-    .incbin ".\strings\talk_m02_jp2.bin"
+    .incbin ".\strings\font_jp\talk_m02_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_m03_jp1.bin", 0
-    .incbin ".\strings\talk_m03_jp1.bin"
+    .incbin ".\strings\font_jp\talk_m03_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_m03_jp2.bin", 0
-    .incbin ".\strings\talk_m03_jp2.bin"
+    .incbin ".\strings\font_jp\talk_m03_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_m04_jp1.bin", 0
-    .incbin ".\strings\talk_m04_jp1.bin"
+    .incbin ".\strings\font_jp\talk_m04_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_m04_jp2.bin", 0
-    .incbin ".\strings\talk_m04_jp2.bin"
+    .incbin ".\strings\font_jp\talk_m04_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_m05_jp1.bin", 0
-    .incbin ".\strings\talk_m05_jp1.bin"
+    .incbin ".\strings\font_jp\talk_m05_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_m05_jp2.bin", 0
-    .incbin ".\strings\talk_m05_jp2.bin"
+    .incbin ".\strings\font_jp\talk_m05_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_m06_jp1.bin", 0
-    .incbin ".\strings\talk_m06_jp1.bin"
+    .incbin ".\strings\font_jp\talk_m06_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_m06_jp2.bin", 0
-    .incbin ".\strings\talk_m06_jp2.bin"
+    .incbin ".\strings\font_jp\talk_m06_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_m07_jp1.bin", 0
-    .incbin ".\strings\talk_m07_jp1.bin"
+    .incbin ".\strings\font_jp\talk_m07_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_m07_jp2.bin", 0
-    .incbin ".\strings\talk_m07_jp2.bin"
+    .incbin ".\strings\font_jp\talk_m07_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_m08_jp1.bin", 0
-    .incbin ".\strings\talk_m08_jp1.bin"
+    .incbin ".\strings\font_jp\talk_m08_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_m08_jp2.bin", 0
-    .incbin ".\strings\talk_m08_jp2.bin"
+    .incbin ".\strings\font_jp\talk_m08_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_m09_jp1.bin", 0
-    .incbin ".\strings\talk_m09_jp1.bin"
+    .incbin ".\strings\font_jp\talk_m09_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_m09_jp2.bin", 0
-    .incbin ".\strings\talk_m09_jp2.bin"
+    .incbin ".\strings\font_jp\talk_m09_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_m0a_jp1.bin", 0
-    .incbin ".\strings\talk_m0a_jp1.bin"
+    .incbin ".\strings\font_jp\talk_m0a_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_m0a_jp2.bin", 0
-    .incbin ".\strings\talk_m0a_jp2.bin"
+    .incbin ".\strings\font_jp\talk_m0a_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_m0b_jp1.bin", 0
-    .incbin ".\strings\talk_m0b_jp1.bin"
+    .incbin ".\strings\font_jp\talk_m0b_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_m0b_jp2.bin", 0
-    .incbin ".\strings\talk_m0b_jp2.bin"
+    .incbin ".\strings\font_jp\talk_m0b_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_m0c_jp1.bin", 0
-    .incbin ".\strings\talk_m0c_jp1.bin"
+    .incbin ".\strings\font_jp\talk_m0c_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_m0c_jp2.bin", 0
-    .incbin ".\strings\talk_m0c_jp2.bin"
+    .incbin ".\strings\font_jp\talk_m0c_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_m0d_jp1.bin", 0
-    .incbin ".\strings\talk_m0d_jp1.bin"
+    .incbin ".\strings\font_jp\talk_m0d_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_m0d_jp2.bin", 0
-    .incbin ".\strings\talk_m0d_jp2.bin"
+    .incbin ".\strings\font_jp\talk_m0d_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_m0e_jp1.bin", 0
-    .incbin ".\strings\talk_m0e_jp1.bin"
+    .incbin ".\strings\font_jp\talk_m0e_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_m0e_jp2.bin", 0
-    .incbin ".\strings\talk_m0e_jp2.bin"
+    .incbin ".\strings\font_jp\talk_m0e_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_m0f_jp1.bin", 0
-    .incbin ".\strings\talk_m0f_jp1.bin"
+    .incbin ".\strings\font_jp\talk_m0f_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_m0f_jp2.bin", 0
-    .incbin ".\strings\talk_m0f_jp2.bin"
+    .incbin ".\strings\font_jp\talk_m0f_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_m10_jp1.bin", 0
-    .incbin ".\strings\talk_m10_jp1.bin"
+    .incbin ".\strings\font_jp\talk_m10_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_m10_jp2.bin", 0
-    .incbin ".\strings\talk_m10_jp2.bin"
+    .incbin ".\strings\font_jp\talk_m10_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_m11_jp1.bin", 0
-    .incbin ".\strings\talk_m11_jp1.bin"
+    .incbin ".\strings\font_jp\talk_m11_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_m11_jp2.bin", 0
-    .incbin ".\strings\talk_m11_jp2.bin"
+    .incbin ".\strings\font_jp\talk_m11_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q00_jp1.bin", 0
-    .incbin ".\strings\talk_q00_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q00_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q00_jp2.bin", 0
-    .incbin ".\strings\talk_q00_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q00_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q01_jp1.bin", 0
-    .incbin ".\strings\talk_q01_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q01_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q01_jp2.bin", 0
-    .incbin ".\strings\talk_q01_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q01_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q02_jp1.bin", 0
-    .incbin ".\strings\talk_q02_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q02_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q02_jp2.bin", 0
-    .incbin ".\strings\talk_q02_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q02_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q03_jp1.bin", 0
-    .incbin ".\strings\talk_q03_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q03_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q03_jp2.bin", 0
-    .incbin ".\strings\talk_q03_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q03_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q04_jp1.bin", 0
-    .incbin ".\strings\talk_q04_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q04_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q04_jp2.bin", 0
-    .incbin ".\strings\talk_q04_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q04_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q05_jp1.bin", 0
-    .incbin ".\strings\talk_q05_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q05_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q05_jp2.bin", 0
-    .incbin ".\strings\talk_q05_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q05_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q06_jp1.bin", 0
-    .incbin ".\strings\talk_q06_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q06_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q06_jp2.bin", 0
-    .incbin ".\strings\talk_q06_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q06_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q07_jp1.bin", 0
-    .incbin ".\strings\talk_q07_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q07_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q07_jp2.bin", 0
-    .incbin ".\strings\talk_q07_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q07_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q08_jp1.bin", 0
-    .incbin ".\strings\talk_q08_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q08_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q08_jp2.bin", 0
-    .incbin ".\strings\talk_q08_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q08_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q09_jp1.bin", 0
-    .incbin ".\strings\talk_q09_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q09_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q09_jp2.bin", 0
-    .incbin ".\strings\talk_q09_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q09_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q0a_jp1.bin", 0
-    .incbin ".\strings\talk_q0a_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q0a_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q0a_jp2.bin", 0
-    .incbin ".\strings\talk_q0a_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q0a_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q0b_jp1.bin", 0
-    .incbin ".\strings\talk_q0b_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q0b_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q0b_jp2.bin", 0
-    .incbin ".\strings\talk_q0b_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q0b_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q0c_jp1.bin", 0
-    .incbin ".\strings\talk_q0c_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q0c_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q0c_jp2.bin", 0
-    .incbin ".\strings\talk_q0c_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q0c_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q0d_jp1.bin", 0
-    .incbin ".\strings\talk_q0d_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q0d_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q0d_jp2.bin", 0
-    .incbin ".\strings\talk_q0d_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q0d_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q0e_jp1.bin", 0
-    .incbin ".\strings\talk_q0e_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q0e_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q0e_jp2.bin", 0
-    .incbin ".\strings\talk_q0e_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q0e_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q0f_jp1.bin", 0
-    .incbin ".\strings\talk_q0f_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q0f_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q0f_jp2.bin", 0
-    .incbin ".\strings\talk_q0f_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q0f_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q10_jp1.bin", 0
-    .incbin ".\strings\talk_q10_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q10_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q10_jp2.bin", 0
-    .incbin ".\strings\talk_q10_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q10_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q11_jp1.bin", 0
-    .incbin ".\strings\talk_q11_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q11_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q11_jp2.bin", 0
-    .incbin ".\strings\talk_q11_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q11_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q12_jp1.bin", 0
-    .incbin ".\strings\talk_q12_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q12_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q12_jp2.bin", 0
-    .incbin ".\strings\talk_q12_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q12_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q13_jp1.bin", 0
-    .incbin ".\strings\talk_q13_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q13_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q13_jp2.bin", 0
-    .incbin ".\strings\talk_q13_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q13_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q14_jp1.bin", 0
-    .incbin ".\strings\talk_q14_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q14_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q14_jp2.bin", 0
-    .incbin ".\strings\talk_q14_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q14_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q15_jp1.bin", 0
-    .incbin ".\strings\talk_q15_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q15_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q15_jp2.bin", 0
-    .incbin ".\strings\talk_q15_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q15_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q16_jp1.bin", 0
-    .incbin ".\strings\talk_q16_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q16_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q16_jp2.bin", 0
-    .incbin ".\strings\talk_q16_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q16_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q17_jp1.bin", 0
-    .incbin ".\strings\talk_q17_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q17_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q17_jp2.bin", 0
-    .incbin ".\strings\talk_q17_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q17_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q18_jp1.bin", 0
-    .incbin ".\strings\talk_q18_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q18_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q18_jp2.bin", 0
-    .incbin ".\strings\talk_q18_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q18_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q19_jp1.bin", 0
-    .incbin ".\strings\talk_q19_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q19_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q19_jp2.bin", 0
-    .incbin ".\strings\talk_q19_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q19_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q1a_jp2.bin", 0
-    .incbin ".\strings\talk_q1a_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q1a_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q1b_jp1.bin", 0
-    .incbin ".\strings\talk_q1b_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q1b_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q1b_jp2.bin", 0
-    .incbin ".\strings\talk_q1b_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q1b_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q1c_jp1.bin", 0
-    .incbin ".\strings\talk_q1c_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q1c_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q1c_jp2.bin", 0
-    .incbin ".\strings\talk_q1c_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q1c_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_q1d_jp1.bin", 0
-    .incbin ".\strings\talk_q1d_jp1.bin"
+    .incbin ".\strings\font_jp\talk_q1d_jp1.bin"
 .close
 
 .create ".\.temp\root\data\talk_q1d_jp2.bin", 0
-    .incbin ".\strings\talk_q1d_jp2.bin"
+    .incbin ".\strings\font_jp\talk_q1d_jp2.bin"
 .close
 
 .create ".\.temp\root\data\talk_sys_jp.bin", 0
-    .incbin ".\strings\talk_sys_jp.bin"
+    .incbin ".\strings\font_jp\talk_sys_jp.bin"
 .close
 
+.loadtable ".\charmap_zxa_cn.txt"
+.include ".\strings\font_ds\m_mg_ds_jp.s"
