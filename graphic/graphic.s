d79aaf1553bf9734cf66822195cba2e2e1557d8a
GitHub Wokann <Kenny_chen1998@outlook.com>
2024-02-05 23:00:53 +0800

    图片部分文件层级调整

--- graphic/graphic.s
+++ graphic/graphic.s
@@ -1,5 +1,5 @@
 .create ".\.temp\root\data\font_cn.bin", 0
-     .incbin ".\graphic\font_cn.bin"
+     .incbin ".\graphic\font_cn\font_cn.bin"
 .close
 
 .open ".\.temp\root\data\font_jp.bin",AddressOfFontJP
@@ -11,21 +11,21 @@ NewFontNameChs:
 .include ".\graphic\ls_map_dtl\ls_map_dtl.s"
 
 .create ".\.temp\root\data\ls_txt_fnt_jp.bin", 0
-    .incbin ".\graphic\ls_txt_fnt_jp.bin"
+    .incbin ".\graphic\ls_txt_fnt_jp\ls_txt_fnt_jp.bin"
 .close
 
 .create ".\.temp\root\data\mg_txt_jp.bin", 0
-    .incbin ".\graphic\mg_txt_jp.bin"
+    .incbin ".\graphic\mg_txt_jp\mg_txt_jp.bin"
 .close
 
 .create ".\.temp\root\data\obj_fnt.bin", 0
-    .incbin ".\graphic\obj_fnt.bin"
+    .incbin ".\graphic\obj_fnt\obj_fnt.bin"
 .close
 
 .create ".\.temp\root\data\sub_oth_jp.bin", 0
-    .incbin ".\graphic\sub_oth_jp.bin"
+    .incbin ".\graphic\sub_oth_jp\sub_oth_jp.bin"
 .close
 
 .create ".\.temp\root\data\title.bin", 0
-    .incbin ".\graphic\title.bin"
+    .incbin ".\graphic\title\title.bin"
 .close
