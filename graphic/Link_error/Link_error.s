678d9b9b5d25eeca50b11fad92945abad7a7e505
GitHub Wokann <Kenny_chen1998@outlook.com>
2024-02-05 23:38:34 +0800

    生成连接错误图片的tile及map

---
+++ graphic/Link_error/Link_error.s
@@ -0,0 +1,24 @@
+.open ".\.temp\root\ftc\arm9.bin", 0x02000000
+
+.org Link_error_tile
+    .incbin ".\graphic\Link_error\Link_error_cn.4bpp.lz"
+
+.org Link_error_pal
+    .incbin ".\graphic\Link_error\Link_error_jp.gbapal"
+
+.org Link_error_map
+    .word 0,0
+    .word (Link_error_map1-InitOfMap1)  ;0x8
+    .word (Link_error_map2-InitOfMap1)  ;0x0610
+
+InitOfMap1:
+    .hword 0,0,32,24    ;宽、高块数
+Link_error_map1:
+    .incbin ".\graphic\Link_error\Link_error_cn-1.map"
+
+InitOfMap2:
+    .hword 0,0,32,24    ;宽、高块数
+Link_error_map2:
+    .incbin ".\graphic\Link_error\Link_error_cn-2.map"
+
+.close
