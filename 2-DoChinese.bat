1f2677fd9fb847def55ce96056cc124e949a5d4f
GitHub Wokann <Kenny_chen1998@outlook.com>
2024-02-28 21:04:30 +0800

    批处理顺序调整

--- 2-DoChinese.bat
+++ 2-DoChinese.bat
@@ -2,26 +2,13 @@
 ::创建汉化rom工作区
 setlocal enabledelayedexpansion
 ::设定临时文件夹位置
-set buildfoldername=.\.temp
-
-::读取解包arm9大小
-for %%A in ("%buildfoldername%\root\ftc\arm9.bin") do set "arm9Size=%%~zA"
-::计算arm压缩大小
-set /a arm9CompressSize=arm9Size - 0xC
-::裁切另存压缩的arm9
-copy "%buildfoldername%\root\ftc\arm9.bin" "%buildfoldername%\root\ftc\arm9de.bin" /Y
-fsutil file setEOF "%buildfoldername%\root\ftc\arm9de.bin" %arm9CompressSize%
-::dd if=%buildfoldername%\root\ftc\arm9.bin of=%buildfoldername%\root\ftc\arm9de.bin bs=1 skip=0 count=%arm9CompressSize%
-::解压压缩的另存arm9
-.\tools\cue\blz.exe -d %buildfoldername%\root\ftc\arm9de.bin
-::另存arm9合并进原arm9
-move /Y %buildfoldername%\root\ftc\arm9de.bin %buildfoldername%\root\ftc\arm9.bin
+set tempfoldername=.\.temp
 
 ::修改arm9及其他汉化资源修改替换
 .\tools\armips\armips.exe .\main_RockmanZXAdvent_Chs.asm
 
 ::压缩arm9
-.\tools\cue\blz.exe -en9 %buildfoldername%\root\ftc\arm9.bin
+.\tools\cue\blz.exe -en9 %tempfoldername%\root\ftc\arm9.bin
 ::修正arm9解压起始位置
 .\tools\armips\armips.exe .\tools\arm9blzoffset.asm
 endlocal
